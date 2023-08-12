77b7704379683d471243c912350c49a7a9b7486c
Miguel Ugsimar <jmjugsimar@gmail.com>
2023-08-12 08:20:33 +0000

    test script

---
+++ server/test/test_app.py
@@ -0,0 +1,30 @@
+import unittest
+import httpx
+
+
+class Test(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        print("setUp")
+
+    async def asyncSetUp(self):
+        print("asyncSetUp")
+        self.client = httpx.Client()
+
+    async def test_response(self):
+        print(self.client)
+        response = self.client.get("http://127.0.0.1:8000")
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.text, '{"message":"Hello World"}')
+
+    def tearDown(self):
+        print("tearDown")
+
+    async def asyncTearDown(self):
+        print("asyncTearDown")
+
+    async def on_cleanup(self):
+        print("cleanup")
+
+
+if __name__ == "__main__":
+    unittest.main()
